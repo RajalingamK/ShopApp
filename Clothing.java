5cf29f8819415c9022071be2e6d019345c922cfc
Rajalingam K <85387525+RajalingamK@users.noreply.github.com>
2021-10-28 18:29:05 +0530

    Add files via upload

---
+++ Clothing.java
@@ -0,0 +1,49 @@
+
+
+/**
+ *
+ * @author RajalingsmK
+ */
+public class Clothing {
+
+    private String description;
+    private double price;
+    private String size = "M";
+
+    public final static double MIN_PRICE = 10.0;
+    public final static double TAX_RATE = 0.2;
+
+    public Clothing(String description, double price, String aSize) {
+        this.description = description;
+        this.price = price;
+        size = aSize;
+    }
+    
+    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        
+        return price + (price * TAX_RATE);
+    }
+
+    public void setPrice(double price) {
+
+        this.price = (price > MIN_PRICE) ? price : MIN_PRICE;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
---
+++ Customer.java
@@ -0,0 +1,77 @@
+
+
+/**
+ *
+ * @author RajalingamK
+ */
+public class Customer {
+    private String name;
+    private String size;
+    
+    private Clothing[] items;
+
+    public Customer(String name, int measurement) {
+        this.name = name;
+        setSize(measurement);
+    }
+    
+    
+    public void addItems(Clothing[] someItems){
+        items = someItems;
+    }
+
+    public Clothing[] getItems(){
+        return items;
+    }
+    
+    public double getTotalClothingCost(){
+        
+        double total = 0.0;
+        
+        for (Clothing item: items) {
+//            if (c1.getSize().equals(item.getSize())){
+                total = total + item.getPrice();
+//                System.out.println("item" + ", " + item.getDescription() + ", " + item.getPrice() + ", " + item.getSize() + ".");
+//                total = total + total * tax;
+////                if (total > 15) {break;}
+////            }
+        }
+    
+        return total;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    
+    public void setSize(int measurement) {
+        switch (measurement) {
+            case 1: case 2: case 3:
+                size = "S";
+                break;
+            
+            case 4: case 5: case 6:
+                size = "M";
+                break;
+            
+            case 7: case 8: case 9:
+                size = "L";
+                break;
+            default:
+                size = "XL";
+        }
+    } 
+    
+}
